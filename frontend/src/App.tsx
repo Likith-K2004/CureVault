c74bc00c5bc3d676e81beb33eae48e8fafbcd2a5
Likith k <likith.li2004@gmail.com>
2025-03-14 12:47:06 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl text-blue-500">
+      Tailwind CSS is Working! 🚀
+    </div>
+  )
+}
+
+export default App
